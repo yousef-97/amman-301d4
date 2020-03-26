ee532b7c6d390147da09f57acf35bfe980558a70
yousef-97 <alshenyousef@gmail.com>
2020-03-26 12:54:17 +0200

    step 10

--- class-00/lab-b/lab/app.js
+++ class-00/lab-b/lab/app.js
@@ -108,12 +108,10 @@ console.log('Hello from the new object function', newObject(['hi', 'hello', 'are
 
 
 
-let sum = function(a, b, c, d) {
-  return a + b + c + d;
-};
+let sum = (a, b, c, d)=>(a + b + c + d);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sum(1, 2, 3, 4));
+console.log(sum(1, 2, 3, 4));
 
 
 let objectLit = function() {
